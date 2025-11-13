354ea25021cce5bd104d5ed8fe6c4f57d5fe4ca7
Maitrii Infotech <maitrii.infotechsolutions@gmail.com>
2025-11-13 14:34:26 +0530

    Branch system

--- src/controllers/branchController.js
+++ src/controllers/branchController.js
@@ -2,6 +2,16 @@ import Branch from "../models/branchModel.js";
 
 export const createBranch = async (req, res) => {
   try {
+    // If only orgId is provided, return branches instead of creating
+    const keys = Object.keys(req.body);
+    if (keys.length === 1 && keys[0] === 'orgId') {
+      const branches = await Branch.find({ orgId: req.body.orgId })
+        .populate('managerId', 'firstName lastName eId')
+        .sort({ createdAt: -1 });
+      return res.status(200).json({ branches });
+    }
+
+    // Otherwise, create a new branch
     const branch = await Branch.create(req.body);
     res.status(201).json({ message: "Branch created", branch });
   } catch (error) {
