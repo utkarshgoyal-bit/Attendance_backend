063c7f45ca58d1aa6f761317ab40232e2b8e16fe
Maitrii Infotech <maitrii.infotechsolutions@gmail.com>
2025-12-11 16:24:03 +0530

    Update

--- server.js
+++ server.js
@@ -6,7 +6,12 @@ require('dotenv').config();
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: 'http://localhost:3000',
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 app.use(express.json());
 
 // MongoDB Connection
