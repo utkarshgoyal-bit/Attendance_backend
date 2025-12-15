5911a0eb2ca181e70c95dd9f3503f3bed60030c4
Maitrii Infotech <maitrii.infotechsolutions@gmail.com>
2025-12-15 17:29:09 +0530

    Update

--- apps/accounts/admin.py
+++ apps/accounts/admin.py
@@ -1,21 +1,18 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
-from .models import User, Organization
-
-
-@admin.register(Organization)
-class OrganizationAdmin(admin.ModelAdmin):
-    list_display = ['name', 'slug', 'is_active', 'created_at']
-    prepopulated_fields = {'slug': ('name',)}
+from .models import User
 
 
 @admin.register(User)
 class UserAdmin(BaseUserAdmin):
-    list_display = ['email', 'username', 'role', 'organization', 'is_staff']
-    list_filter = ['role', 'is_staff', 'is_active', 'organization']
+    list_display = ['email', 'username', 'role', 'organization', 'is_active']
+    list_filter = ['role', 'is_active', 'organization']
+    search_fields = ['email', 'username']
+    
     fieldsets = BaseUserAdmin.fieldsets + (
-        ('Additional Info', {'fields': ('role', 'organization', 'phone', 'is_first_login')}),
+        ('HR System', {'fields': ('role', 'organization', 'phone', 'is_first_login')}),
     )
+    
     add_fieldsets = BaseUserAdmin.add_fieldsets + (
-        ('Additional Info', {'fields': ('email', 'role', 'organization', 'phone')}),
+        ('HR System', {'fields': ('email', 'role', 'organization', 'phone')}),
     )
