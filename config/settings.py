1cf87214014cc27f023afee7adab916b649fb0f8
Maitrii Infotech <maitrii.infotechsolutions@gmail.com>
2025-12-30 13:36:10 +0530

    CLI

--- config/settings.py
+++ config/settings.py
@@ -73,10 +73,8 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'config.wsgi.application'
-# Add this below WSGI_APPLICATION = 'config.wsgi.application'
 ASGI_APPLICATION = 'config.asgi.application'
 
-# Add this at the very bottom of the file
 CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
